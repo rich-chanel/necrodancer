dac93a2ad934c06e7240f5ef1c92208e34f65b77
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-01-22 20:58:47 +0900

    UI 오류 수정

--- twitch/competition.js
+++ twitch/competition.js
@@ -4,6 +4,6 @@ function refresh() {
     var T1 = document.getElementById("1t");
     var T2 = document.getElementById("2t");
     T1.innerHTML = "<iframe src='https://player.twitch.tv/?channel=" + P1 + "&parent=rich-chanel.github.io' height='528' width='938' allowfullscreen='true' style='margin-left: 4px;'></iframe></td>";
-    T2.innerHTML = "<iframe src='https://player.twitch.tv/?channel=" + P2 + "&parent=rich-chanel.github.io' height='528' width='938' allowfullscreen='true' style='margin-left: 4px;'></iframe></td>";
+    T2.innerHTML = "<iframe src='https://player.twitch.tv/?channel=" + P2 + "&parent=rich-chanel.github.io' height='528' width='938' allowfullscreen='true'></iframe></td>";
 
 }
